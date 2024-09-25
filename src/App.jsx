01deec12867e362d8fcca0a093cd6472e524b0cf
Yana Turiak <turyak1998@gmail.com>
2024-09-25 20:10:10 +0100

    persist todos after refreshing using local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [todos, setTodos] = useState([])
   const [todoValue, setTodoValue] = useState('')
 
+  function persistTodos(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+  }
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
+    persistTodos(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -16,6 +20,7 @@ function App() {
     const newTodoList = todos.filter((toDo, i) => {
       return i !== index
     })
+    persistTodos(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -24,6 +29,18 @@ function App() {
     setTodoValue(valueToEdit)
     handleDeleteTodo(index)
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  } , [])
 
   return (
     <>
